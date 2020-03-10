d97663ddcfa8c4ab08953836bd830a0081887a23
Benny Do <gusdocat@student.gu.se>
2020-03-10 08:44:15 +0100

    setting up pieces with initial position for board

--- board.py
+++ board.py
@@ -18,9 +18,43 @@ class Board(chess.Board):
     # Functions
     def createBoard(self):
         for tile in range(64):
-            empty = NullPiece(True, tile)
+            empty = NullPiece(False, tile)
             self.gameTiles[tile] = Tile(tile)
             self.gameTiles[tile].getOccupied(empty)
+        self.gameTiles[0].getOccupied(Rook(False,0))
+        self.gameTiles[1].getOccupied(Knight(False,1))
+        self.gameTiles[2].getOccupied(Bishop(False,2))
+        self.gameTiles[3].getOccupied(Queen(False,3))
+        self.gameTiles[4].getOccupied(King(False,4))
+        self.gameTiles[5].getOccupied(Bishop(False,5))
+        self.gameTiles[6].getOccupied(Knight(False,6))
+        self.gameTiles[7].getOccupied(Rook(False,7))
+        self.gameTiles[8].getOccupied(Pawn(False,8))
+        self.gameTiles[9].getOccupied(Pawn(False,9))
+        self.gameTiles[10].getOccupied(Pawn(False,10))
+        self.gameTiles[11].getOccupied(Pawn(False,11))
+        self.gameTiles[12].getOccupied(Pawn(False,12))
+        self.gameTiles[13].getOccupied(Pawn(False,13))
+        self.gameTiles[14].getOccupied(Pawn(False,14))
+        self.gameTiles[15].getOccupied(Pawn(False,15))
+
+        self.gameTiles[48].getOccupied(Pawn(True,48))
+        self.gameTiles[49].getOccupied(Pawn(True,49))
+        self.gameTiles[50].getOccupied(Pawn(True,50))
+        self.gameTiles[51].getOccupied(Pawn(True,51))
+        self.gameTiles[52].getOccupied(Pawn(True,52))
+        self.gameTiles[53].getOccupied(Pawn(True,53))
+        self.gameTiles[54].getOccupied(Pawn(True,54))
+        self.gameTiles[55].getOccupied(Pawn(True,55))
+        self.gameTiles[56].getOccupied(Rook(True,56))
+        self.gameTiles[57].getOccupied(Knight(True,57))
+        self.gameTiles[58].getOccupied(Bishop(True,58))
+        self.gameTiles[59].getOccupied(Queen(True,59))
+        self.gameTiles[60].getOccupied(King(True,60))
+        self.gameTiles[61].getOccupied(Bishop(True,61))
+        self.gameTiles[62].getOccupied(Knight(True,62))
+        self.gameTiles[63].getOccupied(Rook(True,63))
+
 
 
     def printBoard(self):
